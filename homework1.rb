4aedd976c1e407bbc81bf7d464a03aefd3d28664
Andy Laughlin <atl11384@gmail.com>
2018-04-12 09:49:19 +0100

    edit to homework1 question 10

--- homework1.rb
+++ homework1.rb
@@ -1,20 +1,20 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 stops << "Edinburgh Waverley"
-stops.unshift("Glasgow Queen St")
+stops.unshift("Glasgow Queen St")# pushes item in brackets to start
 stops.insert(4, "Polmont")
 stops.index("Linlithgow")
 stops.delete("Livingston")
 stops.delete_at(2)
 stops.count
 # ["Glasgow Queen St", "Croy", "Falkirk High", "Polmont", "Linlithgow", "Haymarket", "Edinburgh Waverley"]
-stops.fetch(2)
-stops[2]
+stops.fetch(2) #round brackets after the method
+stops[2] # square brackets directly after the array
 
 stops.reverse
 
 for stop in stops
-  return stop
+  print stop
 end
 
 p stops
